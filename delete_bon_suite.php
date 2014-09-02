c2f460662678b4dc0d481ea6790ccc3d2ed489ba
kovalsky <kovalsky@kovalsky>
2014-09-02 21:32:00 +0200

    Suppression étape inutile pour la suppression d'une réservation

--- delete_bon_suite.php
+++ delete_bon_suite.php
@@ -15,17 +15,6 @@ $num_bon=isset($_GET['num_bon'])?$_GET['num_bon']:"";
 $nom=isset($_GET['nom'])?$_GET['nom']:"";
 $billetterie=isset($_GET['billetterie'])?$_GET['billetterie']:"";
 
-$sql = "SELECT sum(quanti)as quanti FROM ".$tblpref."bon_comm BC, ".$tblpref."cont_bon CB 
-		WHERE  CB.bon_num=BC.num_bon
-		AND num_bon = $num_bon";
-$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql2.'<br>'.mysql_error());
-while($data = mysql_fetch_array($req))
-    {
-		$quanti = $data['quanti'];
-		}
-
-if ($quanti==NULL)
-{
 	mysql_select_db($db) or die ("Could not select $db database");
 	$sql1 = "DELETE FROM " . $tblpref ."bon_comm WHERE num_bon = '".$num_bon."'";
 	mysql_query($sql1) or die('Erreur SQL !<br>'.$sql1.'<br>'.mysql_error());
@@ -38,17 +27,7 @@ if ($quanti==NULL)
 		include("lister_commandes.php");
 		echo "tata $billetterie";
 	}
-}
- else {
-	 if ($billetterie!=""){
-		$message = "<center><h1>$lang_err_vider_bon2";
-	 }
-	 else {
-		$message = "<center><h1>$lang_err_vider_bon";
-	 }
+
 include('delete_message.php');
 exit;
-}
-
-
  ?> 
