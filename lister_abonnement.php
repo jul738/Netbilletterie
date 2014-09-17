ac7b34fb0bc606ffd7570ed24c6b876f8907bd12
kovalsky <kovalsky@kovalsky>
2014-09-17 21:11:33 +0200

    Ajout date et heure des spectacles dans la liste des abonnements

--- lister_abonnement.php
+++ lister_abonnement.php
@@ -10,6 +10,7 @@ include_once("include/headers.php");
 include_once("include/head.php");
 include_once("include/finhead.php");
 include_once("include/configav.php");
+include_once("include/fonction.php");
 ?>
 
 <?php
@@ -87,12 +88,35 @@ $liste_abo_vendu = mysql_query($req_liste_abo_vendu) or die('Erreur Req_liste_ab
             </tr>
     </thead>
     <tbody>
-            <tr>
                 <?php
                 $nombre="1";
 		while($data = mysql_fetch_array($liste_abo_vendu))
                 {
-
+                    //On initalise les variables
+                    $choix_spectacle_1 = "";
+                    $choix_spectacle_2 = "";
+                    $choix_spectacle_3 = "";
+                    $choix_spectacle_4 = "";
+                    $choix_spectacle_5 = "";
+                    $choix_spectacle_6 = "";
+                    $choix_spectacle_7 = "";
+                    $date_spectacle_1 = "";
+                    $date_spectacle_2 = "";
+                    $date_spectacle_3 = "";
+                    $date_spectacle_4 = "";
+                    $date_spectacle_5 = "";
+                    $date_spectacle_6 = "";
+                    $date_spectacle_7 = "";
+                    $horaire_spectacle_1 = "";
+                    $horaire_spectacle_2 = "";
+                    $horaire_spectacle_3 = "";
+                    $horaire_spectacle_4 = "";
+                    $horaire_spectacle_5 = "";
+                    $horaire_spectacle_6 = "";
+                    $horaire_spectacle_7 = "";
+                    ?>
+                    <tr>
+                     <?php
                         $num_abo_com = $data['num_abo_com'];
                         $nom = $data['nom'];
                         $prenom = $data['prenom'];
@@ -106,47 +130,89 @@ $liste_abo_vendu = mysql_query($req_liste_abo_vendu) or die('Erreur Req_liste_ab
                         $num_resa_6 = $data['num_resa_6'];
                         $num_resa_7 = $data['num_resa_7'];
                         $commentaire = $data['commentaire'];
-                    if($nombre & 1)
-                        {
-                        $line="0";
-                        }else  
-                            {
-                            $line="1";
+
+                            // ON récupère les nom des spectacles correspondant au numéro
+                            if(!empty($num_resa_1)){
+                            $req_spectacle_1 = 'SELECT article, date_spectacle, horaire FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_1.' AND bc.id_article = article.num';
+                            $sql_spectacle_1 = mysql_query($req_spectacle_1);
+                            while ( $data_spec_1 = mysql_fetch_array($sql_spectacle_1)){
+                                $choix_spectacle_1 = $data_spec_1['article'];
+                                $date_timestamp_1 = strtotime($data_spec_1['date_spectacle']);
+                                $horaire_spectacle_1 = $data_spec_1['horaire'];
+                            }
+                            $date_spectacle_1 = date_fr("l d-m-Y", $date_timestamp_1);
+                            }
+                            
+                            // ON récupère les nom des spectacles correspondant au numéro
+                            if(!empty($num_resa_2)){
+                            $req_spectacle_2 = 'SELECT article, date_spectacle, horaire FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_2.' AND bc.id_article = article.num';
+                            $sql_spectacle_2 = mysql_query($req_spectacle_2);
+                            while ( $data_spec_2 = mysql_fetch_array($sql_spectacle_2)){
+                                $choix_spectacle_2 = $data_spec_2['article'];
+                                $date_timestamp_2 = strtotime($data_spec_2['date_spectacle']);
+                                $horaire_spectacle_2 = $data_spec_2['horaire'];
+                            }
+                            $date_spectacle_2 = date_fr("l d-m-Y", $date_timestamp_2);
+                            }
+                            
+                            // ON récupère les nom des spectacles correspondant au numéro
+                            if(!empty($num_resa_3)){
+                            $req_spectacle_3 = 'SELECT article, date_spectacle, horaire FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_3.' AND bc.id_article = article.num';
+                            $sql_spectacle_3 = mysql_query($req_spectacle_3);
+                            while ( $data_spec_3 = mysql_fetch_array($sql_spectacle_3)){
+                                $choix_spectacle_3 = $data_spec_3['article'];
+                                $date_timestamp_3 = strtotime($data_spec_3['date_spectacle']);
+                                $horaire_spectacle_3 = $data_spec_3['horaire'];
+                            }
+                            $date_spectacle_3 = date_fr("l d-m-Y", $date_timestamp_3);
+                            }
+                            
+                            // ON récupère les nom des spectacles correspondant au numéro
+                            if (!empty($num_resa_4)){
+                            $req_spectacle_4 = 'SELECT article, date_spectacle, horaire FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_4.' AND bc.id_article = article.num';
+                            $sql_spectacle_4 = mysql_query($req_spectacle_4);
+                            while ( $data_spec_4 = mysql_fetch_array($sql_spectacle_4)){
+                                $choix_spectacle_4 = $data_spec_4['article'];
+                                $date_timestamp_4 = strtotime($data_spec_4['date_spectacle']);
+                                $horaire_spectacle_4 = $data_spec_4['horaire'];
+                            }
+                            $date_spectacle_4 = date_fr("l d-m-Y", $date_timestamp_4);
                             }
                             // ON récupère les nom des spectacles correspondant au numéro
-                            $req_spectacle_1 = 'SELECT article FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_1.' AND bc.id_article = article.num';
-                            $sql_spectacle_1 = mysql_query($req_spectacle_1);
-                            $choix_spectacle_1 = mysql_result($sql_spectacle_1,0);
-                            
-                                                        // ON récupère les nom des spectacles correspondant au numéro
-                            $req_spectacle_2 = 'SELECT article FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_2.' AND bc.id_article = article.num';
-                            $sql_spectacle_2 = mysql_query($req_spectacle_2);
-                            $choix_spectacle_2 = mysql_result($sql_spectacle_2,0);
-                            
-                                                        // ON récupère les nom des spectacles correspondant au numéro
-                            $req_spectacle_3 = 'SELECT article FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_3.' AND bc.id_article = article.num';
-                            $sql_spectacle_3 = mysql_query($req_spectacle_3);
-                            $choix_spectacle_3 = mysql_result($sql_spectacle_3,0);
-                            
-                                                        // ON récupère les nom des spectacles correspondant au numéro
-                            $req_spectacle_4 = 'SELECT article FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_4.' AND bc.id_article = article.num';
-                            $sql_spectacle_4 = mysql_query($req_spectacle_4);
-                            $choix_spectacle_4 = mysql_result($sql_spectacle_4,0);
-                            
-                                                        // ON récupère les nom des spectacles correspondant au numéro
-                            $req_spectacle_5 = 'SELECT article FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_5.' AND bc.id_article = article.num';
+                            if (!empty($num_resa_5)){
+                            $req_spectacle_5 = 'SELECT article, date_spectacle, horaire FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_5.' AND bc.id_article = article.num';
                             $sql_spectacle_5 = mysql_query($req_spectacle_5);
-                            $choix_spectacle_5 = mysql_result($sql_spectacle_5,0);
+                            while ( $data_spec_5 = mysql_fetch_array($sql_spectacle_5)){
+                                $choix_spectacle_5 = $data_spec_5['article'];
+                                $date_timestamp_5 = strtotime($data_spec_5['date_spectacle']);
+                                $horaire_spectacle_5 = $data_spec_5['horaire'];
+                            }
+                            $date_spectacle_5 = date_fr("l d-m-Y", $date_timestamp_5);
+                            }
                             
-                                                        // ON récupère les nom des spectacles correspondant au numéro
-                            $req_spectacle_6 = 'SELECT article FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_6.' AND bc.id_article = article.num';
+                            // ON récupère les nom des spectacles correspondant au numéro
+                            if(!empty($num_resa_6)){
+                            $req_spectacle_6 = 'SELECT article, date_spectacle, horaire FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_6.' AND bc.id_article = article.num';
                             $sql_spectacle_6 = mysql_query($req_spectacle_6);
-                            $choix_spectacle_6 = mysql_result($sql_spectacle_6,0);
+                            while ( $data_spec_6 = mysql_fetch_array($sql_spectacle_6)){
+                                $choix_spectacle_6 = $data_spec_6['article'];
+                                $date_timestamp_6 = strtotime($data_spec_6['date_spectacle']);
+                                $horaire_spectacle_6 = $data_spec_6['horaire'];
+                            }
+                            $date_spectacle_6 = date_fr("l d-m-Y", $date_timestamp_6);
+                            }
                             
-                                                        // ON récupère les nom des spectacles correspondant au numéro
-                            $req_spectacle_7 = 'SELECT article FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_7.' AND bc.id_article = article.num';
+                            // ON récupère les nom des spectacles correspondant au numéro
+                            if(!empty($num_resa_7)){
+                            $req_spectacle_7 = 'SELECT article, date_spectacle, horaire FROM article, bon_comm AS bc WHERE bc.num_bon='.$num_resa_7.' AND bc.id_article = article.num';
                             $sql_spectacle_7 = mysql_query($req_spectacle_7);
-                            $choix_spectacle_7 = mysql_result($sql_spectacle_7,0);
+                            while ( $data_spec_7 = mysql_fetch_array($sql_spectacle_7)){
+                                $choix_spectacle_7 = $data_spec_7['article'];
+                                $date_timestamp_7 = strtotime($data_spec_7['date_spectacle']);
+                                $horaire_spectacle_7 = $data_spec_7['horaire'];
+                            }
+                            $date_spectacle_7 = date_fr("l d-m-Y", $date_timestamp_7);
+                            }
                 ?>
 
                         <td> <?php echo $num_abo_com       ; ?> </td>
@@ -154,13 +220,13 @@ $liste_abo_vendu = mysql_query($req_liste_abo_vendu) or die('Erreur Req_liste_ab
                         <td> <?php echo $prenom               ; ?> </td>
                         <td> <?php echo $date              ; ?> </td>
                         <td> <?php echo $nom_abonnement    ; ?> </td>
-                        <td> <?php echo $choix_spectacle_1 ; ?> </td>
-                        <td> <?php echo $choix_spectacle_2 ; ?> </td>
-                        <td> <?php echo $choix_spectacle_3 ; ?> </td>
-                        <td> <?php echo $choix_spectacle_4 ; ?> </td>
-                        <td> <?php echo $choix_spectacle_5 ; ?> </td>
-                        <td> <?php echo $choix_spectacle_6 ; ?> </td>
-                        <td> <?php echo $choix_spectacle_7 ; ?> </td>
+                        <td> <?php echo $choix_spectacle_1 ; ?><br /> <?php echo $date_spectacle_1;?><br /><?php echo $horaire_spectacle_1; ?> </td>
+                        <td> <?php echo $choix_spectacle_2 ; ?><br /> <?php echo $date_spectacle_2;?><br /><?php echo $horaire_spectacle_2; ?> </td>
+                        <td> <?php echo $choix_spectacle_3 ; ?><br /> <?php echo $date_spectacle_3;?><br /><?php echo $horaire_spectacle_3; ?> </td>
+                        <td> <?php echo $choix_spectacle_4 ; ?><br /> <?php echo $date_spectacle_4;?><br /><?php echo $horaire_spectacle_4; ?> </td>
+                        <td> <?php echo $choix_spectacle_5 ; ?><br /> <?php echo $date_spectacle_5;?><br /><?php echo $horaire_spectacle_5; ?> </td>
+                        <td> <?php echo $choix_spectacle_6 ; ?><br /> <?php echo $date_spectacle_6;?><br /><?php echo $horaire_spectacle_6; ?> </td>
+                        <td> <?php echo $choix_spectacle_7 ; ?><br /> <?php echo $date_spectacle_7;?><br /><?php echo $horaire_spectacle_7; ?> </td>
                         <td> <?php echo $commentaire; ?></td>
                         <td><a href='voir_abonnement.php?num_abo_com=<?php echo "$num_abo_com"; ?>' >
                                 <img border="0" alt="voir" src="image/voir.png" Title="Voir les details"></a></td>
@@ -174,15 +240,15 @@ $liste_abo_vendu = mysql_query($req_liste_abo_vendu) or die('Erreur Req_liste_ab
                                 <input type="image" src="image/print.png" style=" border: none; margin: 0;" alt="<?php echo $lang_imprimer; ?>" Title="Imprimer"/>
                             </form>
                          </td>
-                                <td><a href='dupliquer_abonnement.php?num_abo_com=<?php echo "$num_abo_com"; ?>' >
+                                <td><a href='dupliquer_abonnement.php?num_abo_com=<?php echo $num_abo_com; ?>' >
                                                                 <img border="0" alt="voir" src="image/duplicat.png" Title="Dupliquer le spectateur"></a></td>
-                                <td><a href='/fpdf/bon_pdf.php?num_abo_com=<?php echo "$num_abo_com"; ?>'>
+                                <td><a href='/fpdf/bon_pdf.php?num_abo_com=<?php echo $num_abo_com; ?>'>
                                 <img border="0" alt="mail" src="image/mail.png" Title="Envoyer un mail au spectateur"></a></td>
                                 
                 </tr>
           <?php } //Fin du while ?> 
-                </table>
     </tbody>
+</table>
 </table>
 
 
