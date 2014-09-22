1bfbedeeeb8596a2dd30eb279fd23e7f58db6e71
kovalsky <kovalsky@kovalsky>
2014-09-22 11:52:33 +0200

    Ajout des commentaires sur les résas de groupe et sécurisation injection SQL

--- resa_groupe.php
+++ resa_groupe.php
@@ -28,25 +28,26 @@ if (!empty($_POST)){
         $quanti_ancien = $ancien_nb_accompagnateurs + $ancien_nb_enfants;
         $ancien_article = $_POST['ancien_article'];
     }
-    $num_groupe = $_POST['nom-groupe'];
-    $nom_referent = $_POST['nom-referent-groupe'];
+    $num_groupe = mysql_escape_string($_POST['nom-groupe']);
+    $nom_referent = mysql_escape_string($_POST['nom-referent-groupe']);
     $telephone_referent = $_POST['telephone-referent-groupe'];
     $classe_groupe = $_POST['classe-groupe'];
     $nb_enfants = $_POST['nb-enfants'];
     $nb_accompagnateurs = $_POST['nb-accompagnateurs'];
     $nb_gratuit = $_POST['nb-accompagnateurs-gratuit']; 
     $id_article = $_POST['id-article'];
+    $coment = mysql_escape_string($_POST['coment']);
     
     // Insert dans la base
     // Si nouveau alors création de la réservation du groupe
     if(empty($num_resa_groupe)){
-        $sql_insert_resa_groupe = "INSERT INTO " . $tblpref ."bon_comm_groupe(num_groupe, nom_referent, telephone_referent, classe_groupe, nb_enfants, nb_accompagnateurs, nb_gratuit, id_article) VALUES('$num_groupe', '$nom_referent', '$telephone_referent', '$classe_groupe', '$nb_enfants', '$nb_accompagnateurs', '$nb_gratuit', '$id_article')";
+        $sql_insert_resa_groupe = "INSERT INTO " . $tblpref ."bon_comm_groupe(num_groupe, nom_referent, telephone_referent, classe_groupe, nb_enfants, nb_accompagnateurs, nb_gratuit, id_article, coment) VALUES('$num_groupe', '$nom_referent', '$telephone_referent', '$classe_groupe', '$nb_enfants', '$nb_accompagnateurs', '$nb_gratuit', '$id_article', '$coment')";
         $requete_insert_resa_groupe = mysql_query($sql_insert_resa_groupe) or die('Erreur SQL création réservation groupe !<br>'.$requete_insert_resa_groupe.'<br>'.mysql_error());
         $num_resa_groupe = mysql_insert_id();
     }
     // Si existant alors MAJ de la ligne
     else{
-        $sql_update_resa_groupe = "UPDATE " . $tblpref ."bon_comm_groupe SET num_groupe='".$num_groupe."', nom_referent='".$nom_referent."', telephone_referent='".$telephone_referent."', classe_groupe='".$classe_groupe."', nb_enfants='".$nb_enfants."', nb_accompagnateurs='".$nb_accompagnateurs."', nb_gratuit='".$nb_gratuit."', id_article='".$id_article."' WHERE num_bon_groupe='".$num_resa_groupe."' ";
+        $sql_update_resa_groupe = "UPDATE " . $tblpref ."bon_comm_groupe SET num_groupe='".$num_groupe."', nom_referent='".$nom_referent."', telephone_referent='".$telephone_referent."', classe_groupe='".$classe_groupe."', nb_enfants='".$nb_enfants."', nb_accompagnateurs='".$nb_accompagnateurs."', nb_gratuit='".$nb_gratuit."', id_article='".$id_article."', coment='".$coment."' WHERE num_bon_groupe='".$num_resa_groupe."' ";
         $requete_update_resa_groupe = mysql_query($sql_update_resa_groupe) or die('Erreur SQL modification réservation groupe !<br>'.$sql_update_resa_groupe.'<br>'.mysql_error());
     }
     
@@ -116,7 +117,7 @@ elseif(isset($_GET['num_resa_groupe'])){
 // On affiche un groupe
 
 // Récupération des informations de la réservation du groupe
-$sql_select_resa_groupe = "SELECT nom_structure, article, date_spectacle, horaire, nom_referent, telephone_referent, classe_groupe, nb_enfants, nb_accompagnateurs, nb_gratuit, id_article FROM " . $tblpref ."bon_comm_groupe AS bcg, " . $tblpref ."groupe AS g, " . $tblpref ."article AS a WHERE num_bon_groupe='".$num_resa_groupe."' AND bcg.id_article=a.num AND bcg.num_groupe=g.num_groupe";
+$sql_select_resa_groupe = "SELECT nom_structure, article, date_spectacle, horaire, nom_referent, telephone_referent, classe_groupe, nb_enfants, nb_accompagnateurs, nb_gratuit, id_article, coment FROM " . $tblpref ."bon_comm_groupe AS bcg, " . $tblpref ."groupe AS g, " . $tblpref ."article AS a WHERE num_bon_groupe='".$num_resa_groupe."' AND bcg.id_article=a.num AND bcg.num_groupe=g.num_groupe";
 $requete_select_resa_groupe = mysql_query($sql_select_resa_groupe) or die('Erreur SQL sélection groupe !<br>'.$sql_select_resa_groupe.'<br>'.mysql_error());
 while($data_resa_groupe = mysql_fetch_array($requete_select_resa_groupe)){
     $nom_structure = $data_resa_groupe['nom_structure'];
@@ -129,6 +130,7 @@ while($data_resa_groupe = mysql_fetch_array($requete_select_resa_groupe)){
     $nb_enfants = $data_resa_groupe['nb_enfants'];
     $nb_accompagnateurs = $data_resa_groupe['nb_accompagnateurs'];
     $nb_gratuit = $data_resa_groupe['nb_gratuit'];
+    $coment = $data_resa_groupe['coment'];
 }
 
 $date_article = date_fr("l d-m-Y", $date);
@@ -143,6 +145,7 @@ $date_article = date_fr("l d-m-Y", $date);
     <div id="classe-groupe">Classe / Age du groupe : <?php echo $classe_groupe; ?></div>
     <div id="nb_enfants">Nombre d'enfants : <?php echo $nb_enfants; ?></div>
     <div id="nb-accompagnateurs">Nombres d'accompagnateurs : <?php echo $nb_accompagnateurs; ?> dont gratuit : <?php echo $nb_gratuit; ?></div>
+    <div id="comentaire-resa-groupe">Commentaire de la réservation : <?php echo $coment; ?></div>
 </div>
 <?php
 include("include/bas.php");
