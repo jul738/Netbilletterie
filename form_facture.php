a8ef7f88353c96bd3f995e59b92af84209f3d313
kovalsky <kovalsky@debian>
2014-10-07 11:43:21 +0200

    Formulaire de modification des factures

--- form_facture.php
+++ form_facture.php
@@ -19,6 +19,18 @@ include_once("include/finhead.php");
 if(isset($_GET['num_facture'])){
     $num_facture = $_GET['num_facture'];
     //On fait une requête pour récupérer les infos
+    $select_facture = "SELECT numero_facture, num_groupe, id_resa, type_facture, type_resa, commentaire, id_paiement, date_paiement FROM facture WHERE id = ".$num_facture."";
+    $result_facture = mysql_query($select_facture) or die ('Erreur sélection facture');
+    while($data_facture = mysql_feth_array($result_facture)){
+        $numero_facture = $data_facture['numero_facture'];
+        $num_groupe = $data_facture['num_groupe'];
+        $id_resa = $data_facture['id_resa'];
+        $type_facture = $data_facture['type_facture'];
+        $type_resa = $data_facture['type_resa'];
+        $commentaire = $data_facture['commentaire'];
+        $moyen_paiement = $data_facture['id_paiement'];
+        $date_paiement = $data_facture['date_paiement'];
+    }
 }
 //Sinon on récupère les infos pour créer la facture
 else {
@@ -33,7 +45,7 @@ else {
 <div id="form-facture">
         <h3><?php if(!isset($_GET['num_facture'])){echo "Formulaire de création d'une facture pour le spectacle et pour le client";} else { echo "Formulaire de modification d'une facture pour le spectacle et pour le client";}?></h3>
     <form action="facture.php" method="post" id="form-facture">
-        <label for="numero-facture">Numéro de facture</label><input type="text" name="numero-facture" id="numero-facture"></input><br />
+        <label for="numero-facture">Numéro de facture</label><input type="text" name="numero-facture" id="numero-facture"><?php if(!empty($numero_facture)){ echo $numero_facture;}?></input><br />
         <label for="type-facture">Type de facture</label><select id="type-facture" name="type-facture">
             <option value="acompte">Acompte</option>
             <option value="finale">Finale</option>
